4db0d3a1c8143a2d3a32544bd2e31477c723c55f
m.chorakchi@ymail.com <m.chorakchi@ymail.com>
2018-10-28 10:57:33 +0330

    build

---
+++ build/precache-manifest.b3cc6964c911a0b197616bb0d66de781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de87cf8598604a642520",
+    "url": "/static/js/main.de87cf85.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "de87cf8598604a642520",
+    "url": "/static/css/main.d5fc5207.chunk.css"
+  },
+  {
+    "revision": "13c1e39b460b94b467e4794e2d90cad7",
+    "url": "/index.html"
+  }
+];
