7c17a93e109a68091f00335a62e90cf64776b6fa
mochorak <mohammad.chorakchi@akelius.de>
2020-09-13 02:41:18 +0200

    init hotjar

--- src/Countiner/App.js
+++ src/Countiner/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import Main from "./Main";
+import { hotjar } from "react-hotjar";
+hotjar.initialize(1718986, 6);
 
 function App() {
-  return (
-    <Main/>
-  );
+  return <Main />;
 }
 
 export default App;
