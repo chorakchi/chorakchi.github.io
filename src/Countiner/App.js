458bf3b1517cb319fcba6a0f3c9edae18a65f4f5
mochorak <mohammad.chorakchi@akelius.de>
2020-09-13 02:48:24 +0200

    new id for hotjar confing

--- src/Countiner/App.js
+++ src/Countiner/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Main from "./Main";
 import { hotjar } from "react-hotjar";
-hotjar.initialize(1718986, 6);
+hotjar.initialize(1988903, 6);
 
 function App() {
   return <Main />;
