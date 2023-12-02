95a488df926d7d19dfadafe2b00a5e7c62255b79
chorakchi <m.chorakchi@gmail.com>
2023-12-02 17:56:41 +0100

    test for auto deplyment

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          test for auto deplyment
         </a>
       </header>
     </div>
