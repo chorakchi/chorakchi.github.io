c28ce753bcdbad3ede860f080fb581db66729a60
chorakchi <m.chorakchi@gmail.com>
2023-12-07 02:08:18 +0100

    add missing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, useLocation } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
 function App() {
--- src/components/atoms/Box/Box.styled.tsx
+++ src/components/atoms/Box/Box.styled.tsx
@@ -7,9 +7,9 @@ type Index = {
   };
   
   const index: any = (obj:Index, is:any, value:any) => {
-    if (typeof is == 'string') return index(obj, is.split('.'), value);
-    else if (is.length == 1 && value !== undefined) return (obj[is[0]] = value);
-    else if (is.length == 0) return obj;
+    if (typeof is === 'string') return index(obj, is.split('.'), value);
+    else if (is.length === 1 && value !== undefined) return (obj[is[0]] = value);
+    else if (is.length === 0) return obj;
     else return index(obj[is[0]], is.slice(1), value);
 };
 
